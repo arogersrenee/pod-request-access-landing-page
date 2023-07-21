a4d219c1db494bc541208acedc0f60c4d554403d
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-07-21 16:45:03 -0500

    minor code clean up

--- script.js
+++ script.js
@@ -2,17 +2,10 @@ const accessBtn = document.getElementById('submit-button');
 const email = document.getElementById('email');
 const form = document.getElementById('form');
 const error = document.createElement("p");
-const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-// accessBtn.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     errorMessage();
-//     })
+const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //to validate email format against
 
 function formValidation (){
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        console.log("Screen width is at least 768");
-
+    if (window.matchMedia("(min-width: 768px)").matches) { // error msg location based on the screen width
         if(email.value === "") {
             insertAfter(error, form);
             error.innerText = "Oops! Please add your email";
@@ -30,30 +23,27 @@ function formValidation (){
                 alert('Access Granted')
             }
         }    
-    else {
-        console.log("Screen less than 500px");
+    else { // if screen is less than 768px
         if(email.value === ''){
             insertAfter(error, email);
             error.classList.add('error');
             error.style.marginBottom =  "20px";
             form.style.gap = 0;
             error.innerText = "Oops! Please add your email";   
-        } else if(!email.value.trim().match(emailFormat)){
-            insertAfter(error, email);
-            error.classList.add('error');
-            error.style.marginBottom =  "20px";
-            form.style.gap = 0;
-            error.innerText = "Oops! Please check your email"; 
-        } else {
-            console.log('great job')
-            error.style.display = "none"; 
-            form.style.gap = "1rem";
-            alert('Access Granted')
-
-        }
+            } else if(!email.value.trim().match(emailFormat)){
+                insertAfter(error, email);
+                error.classList.add('error');
+                error.style.marginBottom =  "20px";
+                form.style.gap = 0;
+                error.innerText = "Oops! Please check your email"; 
+                } else {
+                    console.log('great job')
+                    error.style.display = "none"; 
+                    form.style.gap = "1rem";
+                    alert('Access Granted')
+                }
     }
-    }
-
+}
 
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
