ddc634d61cccb0f5346eb1d517efba054c146cc9
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-07-21 16:04:16 -0500

    update formValidation function in js - html: changed input types to text and removed 'required' to remove the built-in validation. Not sure if this is best practice. - html: invoked the formValidation function inline followed by return false. this seemed to help prevent the form from running and getting an error screen after submit. - js added/updated the else if statements to include email validation.

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 const accessBtn = document.getElementById('submit-button');
 const email = document.getElementById('email');
 const form = document.getElementById('form');
-const emailInput = document.forms['email-form']["email"].value
 const error = document.createElement("p");
-const formContainer = document.getElementsByClassName("section-form")
+const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+// accessBtn.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     errorMessage();
+//     })
 
-
-accessBtn.addEventListener('click', () => {
-    
-    errorMessage();
-
-    })
-
-
-function errorMessage (){
-
+function formValidation (){
     if (window.matchMedia("(min-width: 768px)").matches) {
-    console.log("Screen width is at least 768");
-    insertAfter(error, form)
-    error.innerText = "Oops! Please add your email";
-    error.classList.add('error');
-    error.style.margin =  "0 px;";
-    } else {
-        if(emailInput === ""){
-            console.log("Screen less than 500px");
+        console.log("Screen width is at least 768");
+
+        if(email.value === "") {
+            insertAfter(error, form);
+            error.innerText = "Oops! Please add your email";
+            error.classList.add('error');
+            error.style.margin =  "0 px;";
+            } else if(!email.value.trim().match(emailFormat)){
+                insertAfter(error, form);
+                error.innerText = "Oops! Please check your email";
+                error.classList.add('error');
+                error.style.margin =  "0 px;";
+            } else {
+                console.log('great job')
+                error.style.display = "none"; 
+                form.style.gap = "1rem";
+                alert('Access Granted')
+            }
+        }    
+    else {
+        console.log("Screen less than 500px");
+        if(email.value === ''){
             insertAfter(error, email);
             error.classList.add('error');
             error.style.marginBottom =  "20px";
             form.style.gap = 0;
             error.innerText = "Oops! Please add your email";   
-            
+        } else if(!email.value.trim().match(emailFormat)){
+            insertAfter(error, email);
+            error.classList.add('error');
+            error.style.marginBottom =  "20px";
+            form.style.gap = 0;
+            error.innerText = "Oops! Please check your email"; 
+        } else {
+            console.log('great job')
+            error.style.display = "none"; 
+            form.style.gap = "1rem";
+            alert('Access Granted')
+
         }
     }
-}
+    }
+
 
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
